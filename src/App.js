3ba74202c6faa315dd7bf2c31bc44b7473fad14b
TotoroDavid <davidtoro65@hotmail.com>
2021-04-11 10:33:44 -0500

    setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Typography, AppBar, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello world!!</h1>
-        </div>
+        <>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <photoCameraIcon />
+                    <Typography variant='h6'> 
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth='sm'>
+                        <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+                            photoAlbum
+                        </Typography>
+                        <Typography variant='h5' align='center' color='textSecondary'  paragraph>
+                            Hello everyOne this is a photoAlbum and I'm trying to make this sentence as long as possible so we can see how it look like on the screen
+                        </Typography>
+                    </Container>
+                </div>
+            </main>
+        </>
     )
 }
 
